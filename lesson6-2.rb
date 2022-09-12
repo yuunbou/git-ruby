c14574593a682f3eb555198f43d33a7ba3ee58e1
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-09-12 04:19:14 +0000

    [Pause] Ruby6章演習

---
+++ lesson6-2.rb
@@ -0,0 +1,19 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+
+input = gets.to_i
+
+i = 1
+while <= input do
+  puts "#{i}回目の計算"
+  puts "２つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+
+end
